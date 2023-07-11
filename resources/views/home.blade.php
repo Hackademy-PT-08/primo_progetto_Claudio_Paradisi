5658620228178fa62c7ec6279aa4d7e536456923
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-07-11 20:51:44 +0200

    primo progetto laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/my-style.css">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg bg-dark fixed-top mb-2" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Laravel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/pagina1">Pagina 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/pagina2">Pagina 2</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="/">Home</a>
+              <a class="dropdown-item" href="/pagina1">Pagina 1</a>
+              <a class="dropdown-item" href="/pagina2">Pagina 2</a>
+            <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="https://github.com/Hackademy-PT-08/primo_progetto_caterina_perrucci/blob/main">Repository di Caterina</a>
+            </div>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-sm-2" type="search" placeholder="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <header>
+    <div class="container-fluid mt-5 title-img p-0">
+      <div class="">
+        <div class="col-12 text-center ">
+          <h1 class="h1 text-white mt-5">La Homepage del Primo Progetto Laravel</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <div class="container m-5">
+      <div class="row">
+        <div class="col-12">
+          <h2>Learning Laravel in progress</h2>
+        <div class="progress">
+          <div class="progress-bar bg-dark progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 15%;">LONG WAY TO GO...</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container m-5">
+      <div class="row">
+        <div class="col-12 col-lg-4 mb-5">
+          <div class="list-group ">
+            <a href="#" class="list-group-item list-group-item-dark list-group-item-action flex-column align-items-start active">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-5">
+          <div class="list-group ">
+            <a href="#" class="list-group-item list-group-item-dark list-group-item-action flex-column align-items-start active">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-5">
+          <div class="list-group ">
+            <a href="#" class="list-group-item list-group-item-dark list-group-item-action flex-column align-items-start active">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-5">
+          <div class="list-group ">
+            <a href="#" class="list-group-item list-group-item-dark list-group-item-action flex-column align-items-start active">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container-fluid p-5 bg-dark text-white">
+        <div class="row">
+          <div class="col-12 col-lg-4">
+            <p class="h4 m-3">Contatti</p>
+            <p class="h4 m-3">About us</p>
+            <p class="h4 m-3">Lavora con noi</p>
+          </div>
+          <div class="col-12 col-lg-4">
+            <p class="h4 m-3">link utili</p>
+            <p class="h4 m-3">Aulab</p>
+            <p class="h4 m-3">Laravel</p>
+          </div>
+          <div class="col-12 col-lg-4">
+            <p class="h4 m-3">Php</p>
+            <p class="h4 m-3">Javasript</p>
+            <p class="h4 m-3">Css3</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="/my-js/my-script.js"></script>
+  </body>
+</html>
